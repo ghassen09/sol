ee445e62c576ab0ae3e6365fb3e83d052a9f2d58
houssemhannachi <72234639+houssemhannachi@users.noreply.github.com>
2021-08-09 10:21:44 +0100

    Update clients.php

--- clients.php
+++ clients.php
@@ -139,7 +139,7 @@ if (isset($_SESSION['id']) && isset($_SESSION['user_name'])) {
 				<div class="modal-dialog modal-dialog-centered modal-lg" role="document">
 					<div class="modal-content">
 						<div class="modal-header">
-							<h5 class="modal-title">Ajouter</h5>
+							<h5 class="modal-title">Ajouter un Client</h5>
 							<button type="button" class="close" data-dismiss="modal" aria-label="Close">
 								<span aria-hidden="true">&times;</span>
 							</button>
