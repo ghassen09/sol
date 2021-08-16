9094a07a3bf4652db3e4e0f5cc7c7d53a963def7
ghassen09 <67017527+ghassen09@users.noreply.github.com>
2021-08-16 14:29:23 +0200

    Create ajouter_fact.php

---
+++ ajouter_fact.php
@@ -0,0 +1,50 @@
+<?php
+require_once ("db_conn.php");
+
+if (isset($_POST['submit'])){
+    if (empty($_POST['id_client'])|| empty($_POST['reference'])|| empty($_POST['designation'])|| empty($_POST['date'])|| empty($_POST['quantite'])|| empty($_POST['puht'])|| empty($_POST['montantht']) )
+    {
+        echo "Please fill the blanks";
+    }
+    else {
+        $client = $_POST['id_client'];
+        $reference = $_POST['reference'];
+        $designation = $_POST['designation'];
+        $date = $_POST['date'];
+        $quantite = $_POST['quantite'];
+        $puht = $_POST['puht'];
+        $montantht = $_POST['montantht'];
+      
+        
+
+        $query = "Insert into facture (id_client,reference_facture,designation_facture,date_facture,quantite_facture,puht_facture,montantht_facture) values ('$client','$reference','$designation','$date','$quantite','$puht','$montantht')";
+        $result = mysqli_query($conn,$query);
+
+        if($result) {
+            header ("location:facture.php");
+
+        }
+        else 
+        {
+            echo "Please check your query";
+
+        }
+
+
+    }
+
+}
+else
+{
+    header ('location:facture.php');
+
+}
+
+
+
+
+
+
+
+
+?>
